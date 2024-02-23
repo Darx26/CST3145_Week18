cc77626111268a5e4b9d411a04b222d71c31bb58
Hayden <hayden26@live.com>
2024-02-23 15:36:36 +0400

    final service worker js file

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+var cacheName = 'petstore0-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-house',
+    'images/cat.jpg',
+    'images/litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/math.png',
+    'images/math.jpg',
+    'images/icon-store-512.png'
+]
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all files');
+            return cache.addAll(cacheFiles);
+        })
+    )
+})
+
+self.addEventListener('fetch', function (e){
+    e.respondWith(
+        //check if the cache has the file
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+
+            //'r' is the matching file if it exists in the catch
+            return r
+        })
+    )
+} )
